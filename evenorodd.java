412b0236157353699167ef3ec8342c6299746eda
Sireesha1602 <r170150@rguktrkv.ac.in>
2024-02-28 13:54:24 +0530

    evenorodd.java is added

---
+++ evenorodd.java
@@ -0,0 +1,12 @@
+public class EvenOrOdd
+{
+   public static void main(String[] args) 
+     {
+          System.out.println("enter a number");
+         int n=sc.nextInt();
+         if(n%2==0)
+           System.out.println(n+"is even");
+         else
+           System.out.printl(n+"is odd");
+     }
+}
